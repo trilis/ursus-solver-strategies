cf238b2deeb688fe6080b24dad913ad7a1863677
Alexey Trilis <aatrilis2000@gmail.com>
2025-04-12 14:29:47 +0000

    new_top_down_solver - stable!

--- src/CommonTactics.v
+++ src/CommonTactics.v
@@ -59,37 +59,80 @@ End WithLedger.
 Module ContractTactics (WL: WithLedger).
 Import WL.
 
+Ltac new_top_down_solver' :=
+  clear_unneeded_hyps; repeat match reverse goal with
+    | H: ?y = ?t |- _ =>
+    is_var y;
+    match type of y with
+      | Ledger => fail 1
+      | _ => subst y
+    end
+    end;
+    repeat match reverse goal with 
+      | H: ?y = ?t |- _ => 
+        is_var y; lazy in H; 
+        match t with 
+          | if ?b then _ else _ =>
+            destruct b
+          | _ => subst y
+        end
+  end; lazy; auto.
 
-Ltac new_top_down_solver' Ledger :=
-  clear_unneeded_hyps; repeat match reverse goal with 
-      | H: ?y = ?t |- _ =>
-      assert_fails (idtac; multimatch goal with 
+(* for ifs *)
+(*time (clear_unneeded_hyps; repeat match reverse goal with
+    | H: ?y = ?t |- _ =>
+    is_var y;
+    match t with 
+      | if ?b then _ else _ =>
+        fail 1 
+      | _ =>
+        match type of y with
+          | LedgerLRecord rec => fail 1
+          | _ => subst y
+        end
+    end
+  end;
+  repeat match reverse goal with 
+      | H: ?y = ?t |- _ => 
+        is_var y;
+        match type of t with 
+        | LedgerLRecord rec => idtac 
+        | _ => fail 1
+        end;
+        assert_fails (idtac; multimatch goal with 
           | H2: ?y2 = _ |- _ => 
               match t with 
                   | context[y2] => idtac
                   | _ => fail
               end
-      end);
-      idtac y; (match t with 
-          | if ?b then _ else _ => lazy in H; match reverse goal with
-            | H' : y = if ?b' then _ else _ |- _ => destruct b'
-            end
-          | _ => idtac
-      end); 
-      (match type of y with
-        | Ledger => idtac "LEDGER"; lazy in H; subst y
-        | ULValue _ => idtac "ULVALUE"; subst y
-        | _ =>
-      match reverse goal with 
-        | H2: ?y2 = ?t2 |- _ =>
-          match t2 with
-            | context[y] => (rewrite H in H2; clear H; clear y; idtac "EASY") ||
-              (lazy in H; subst y)
-            | _ => fail
-          end
-      end
-      end) || (lazy in H; subst y)
-  end; lazy; auto.
+      end); idtac y; lazy in H; subst y
+  end;
+  repeat match reverse goal with 
+    | H: ?y = ?t |- _ => 
+        is_var y;
+        match t with 
+        | if ?b then _ else _ => 
+          idtac y;
+          let b' := fresh "b'" in
+          let Heqb' := fresh "Heqb'" in
+          remember b as b' eqn:Heqb'; lazy in Heqb'; 
+          match type of Heqb' with
+            | b' = ?t => destruct t 
+          end; subst b'
+        | _ => fail 1
+        end
+  end;
+  repeat match reverse goal with 
+      | H: ?y = ?t |- _ => 
+        is_var y;
+        assert_fails (idtac; multimatch goal with 
+          | H2: ?y2 = _ |- _ => 
+              match t with 
+                  | context[y2] => idtac
+                  | _ => fail
+              end
+      end); idtac y; lazy in H; subst y
+  end; lazy; auto).*)
 
 Ltac new_top_down_solver := let Ledger' := eval cbv delta [Ledger] in Ledger in new_top_down_solver' Ledger'.
 Ltac bottom_up_goal_solver := let Ledger' := eval cbv delta [Ledger] in Ledger in bottom_up_goal_solver' Ledger'.
@@ -112,13 +155,6 @@ End ContractTactics.
 Ltac top_down_solver :=
   clear_unneeded_hyps; repeat match reverse goal with 
       | H: ?y = ?t |- _ =>
-      assert_fails (idtac; multimatch goal with 
-          | H2: ?y2 = _ |- _ => 
-              match t with 
-                  | context[y2] => idtac
-                  | _ => fail
-              end
-      end);
       idtac y; (match t with 
           | context[if ?b then _ else _] => destruct b
           | _ => idtac
@@ -134,13 +170,6 @@ Ltac let_form_solver :=
 clear_unneeded_hyps; repeat match reverse goal with 
     | H: ?y = ?t |- _ =>
     is_var y; 
-    assert_fails (idtac; multimatch goal with 
-        | H2: ?y2 = _ |- _ => 
-            match t with 
-                | context[y2] => idtac
-                | _ => fail
-            end
-    end);
     match t with 
     | if ?b then _ else _ => 
       let b' := fresh "b'" in
@@ -158,3 +187,86 @@ end; lazy; auto.
 Axiom false_: False.
 
 Ltac abort := exfalso; exact false_.
+
+Definition wrapper {T: Type} (x: T) : True := I.
+Inductive meta :=
+  | meta_y: True -> Datatypes.list True -> Datatypes.list True -> meta.
+
+Ltac do_rewrites m H :=
+  match m with
+    | Datatypes.cons (wrapper ?y2) ?ms =>
+      match goal with 
+        | H2: ?y2 = _ |- _ =>
+          rewrite H in H2; do_rewrites ms H  
+      end
+    | Datatypes.nil => idtac
+  end.
+
+(*Ltac topdown_graph :=
+  repeat match goal with 
+    | _: ?y1 = ?t1 |- _ =>
+      is_var y1;
+      match goal with
+        | _ := meta_y (wrapper y1) _ _ |- _ => fail 1
+        | |- _ => idtac
+      end;
+      set (l := @Datatypes.nil True);
+      set (l2 := @Datatypes.nil True);
+      repeat match goal with 
+        | H2: ?y2 = ?t2 |- _ =>
+          is_var y2;
+          match eval cbv delta [l] in l with 
+            | context[y2] => fail 1
+            | _ => idtac
+          end;
+          match t1 with 
+            | context[y2] =>
+              set (l' := Datatypes.cons (wrapper y2) l);
+              subst l; rename l' into l
+          end
+      end;
+      repeat match goal with 
+        | H2: ?y2 = ?t2 |- _ =>
+          is_var y2;
+          match eval cbv delta [l2] in l2 with 
+            | context[y2] => fail 1
+            | _ => idtac
+          end;
+          match t2 with 
+            | context[y1] =>
+              set (l2' := Datatypes.cons (wrapper y2) l2);
+              subst l2; rename l2' into l2
+          end
+      end;
+      pose (meta_y (wrapper y1) l l2);
+      subst l; subst l2
+  end;
+  clear_unneeded_hyps; repeat match reverse goal with 
+      | H: ?y = ?t |- _ =>
+      assert_fails (idtac; multimatch goal with 
+          | H2: ?y2 = _ |- _ => 
+              match t with 
+                  | context[y2] => idtac
+                  | _ => fail
+              end
+      end);
+      idtac y; (match t with 
+          | if ?b then _ else _ => destruct b
+          | _ => idtac
+      end); 
+      match type of y with
+        | LedgerLRecord rec => cbv in H; subst y
+        | _ => 
+          match goal with
+          | _ := meta_y (wrapper y) _ (Datatypes.nil) |- _ =>
+            repeat rewrite H; clear H
+          | _ : meta_y (wrapper y) _ (Datatypes.cons ?y2 Datatypes.nil) |- _ =>
+            match reverse goal with
+              | H2: ?y2 = _ |- _ =>
+                rewrite H in H2; clear H
+            end
+          | _ := meta_y (wrapper y) _ ?l |- _ =>
+            subst y
+          end
+      end
+  end; lazy; auto.*)
